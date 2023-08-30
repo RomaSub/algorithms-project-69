5177adefdfaf6f6052e018a458299eb37176b122
RomaSub <antonfrolov1212@gmail.com>
2023-08-31 02:32:39 +0300

    провека 2

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -1,15 +1,14 @@
-import { describe, expect, test } from '@jest/globals';
 import search from '../src/index.js';
 
-describe('Поиск', () => {
+describe('Поиск', () => { //eslint-disable-line
   const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
   const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
   const doc3 = { id: 'doc3', text: "I'm your shooter." };
   const docs = [doc1, doc2, doc3];
 
-  test('шаг 1, поиск оп слову', () => {
+  test('шаг 1, поиск по слову', () => { //eslint-disable-line
     const result = search(docs, 'shoot');
 
-    expect(result).toEqual([doc1.id, doc2.id]);
+    expect(result).toEqual([doc1.id, doc2.id]); //eslint-disable-line
   });
 });

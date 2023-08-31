37d34972b4aa15cffaac190ccad107085fe85dee
RomaSub <antonfrolov1212@gmail.com>
2023-08-31 23:33:20 +0300

    шаг 6, тест

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const search = (arraydocs) => {
-  const index = arraydocs.reduce((acc, { id, text }) => {
+const search = (arrayDocs) => {
+  const index = arrayDocs.reduce((acc, { id, text }) => {
     const keys = text.match(/\w+/g);
     keys.forEach((word) => {
       if (acc[word]) {
@@ -11,6 +11,34 @@ const search = (arraydocs) => {
     return acc;
   }, {});
 
-  return index;
+  const documentCount = arrayDocs.length;
+  const idf = {};
+  Object.keys(index).forEach((word) => {
+    const documentFrequency = index[word].length;
+    idf[word] = Math.log(documentCount / (1 + documentFrequency));
+  });
+
+  const tfIdfIndex = {};
+  arrayDocs.forEach(({ id, text }) => {
+    const keys = text.match(/\w+/g);
+    const wordFrequency = {};
+    keys.forEach((word) => {
+      wordFrequency[word] = (wordFrequency[word] || 0) + 1;
+    });
+
+    Object.keys(wordFrequency).forEach((word) => {
+      const tf = wordFrequency[word] / keys.length;
+      const tfIdf = tf * idf[word];
+      if (tfIdfIndex[word]) {
+        tfIdfIndex[word][id] = tfIdf;
+      } else {
+        tfIdfIndex[word] = { [id]: tfIdf };
+      }
+    });
+  });
+
+  console.log(tfIdfIndex);
+  return tfIdfIndex;
 };
+
 export default search;

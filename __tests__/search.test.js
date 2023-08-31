d4293542640bda218939654a708b079e79458482
RomaSub <antonfrolov1212@gmail.com>
2023-08-31 04:02:31 +0300

    шаг 3

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -6,11 +6,11 @@ describe('Поиск', () => { //eslint-disable-line
   const doc3 = { id: 'doc3', text: "I'm your shooter." };
   const docs = [doc1, doc2, doc3];
 
-  test('шаг 1, Поиск по слову', () => { //eslint-disable-line
-    const result = search(docs, 'shoot');
-
-    expect(result).toEqual([doc1.id, doc2.id]); //eslint-disable-line
-  });
+  // test('шаг 1, Поиск по слову', () => { //eslint-disable-line
+  //   const result = search(docs, 'shoot');
+  //
+  //   expect(result).toEqual([doc1.id, doc2.id]); //eslint-disable-line
+  // });
 
   test('шаг 2, Обработка текста', () => { //eslint-disable-line
     const result = search(docs, 'pint');
@@ -18,5 +18,11 @@ describe('Поиск', () => { //eslint-disable-line
 
     expect(result).toEqual([doc1.id]); //eslint-disable-line
     expect(result2).toEqual([doc1.id]); //eslint-disable-line
+  });
+
+  test('шаг 3, Релевантность', () => { //eslint-disable-line
+    const result = search(docs, 'shoot');
+
+    expect(result).toEqual([doc2.id, doc1.id]); //eslint-disable-line
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const search = (arrayDocs, searchString) => {
   const term = searchString.match(/\w+/g)[0];
-  const result = arrayDocs
-    .filter(({ text }) => text.match(/\w+/g).includes(term))
-    .map(({ id }) => id);
-  return result;
+  const result = [];
+  arrayDocs.forEach(({ id, text }) => {
+    const matches = text.match(/\w+/g).filter((word) => word === term);
+    if (matches.length) {
+      result.push({ id, relevance: matches.length });
+    }
+  });
+
+  result.sort((a, b) => b.relevance - a.relevance);
+  console.log(result);
+  return result.map(({ id }) => id);
 };
 export default search;
